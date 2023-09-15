50b35b50bfe668f3b8906e689878312c599c2d8c
vijay41720 <vijaykumar41720@gmail.com>
2023-09-15 12:31:49 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar(){
+  return <img src="MyPic.jpg" alt="Vijay" className="avatar"/>
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>Vijaykumar Tirupathi</h1>
+      <p>
+      Over 1.8 years of experience in the field of testing. Experience in Manual testing on Web and Desktop based applications.
+      And I am having the handsone experience as a Full-stack web developer. Looking to switch the domain from the testing field to the
+      Full-stack developer field. 
+      </p>
+    </div>
+  );
+}
+
+function SkillList(){
+  return(
+    <div className="skill-list">
+      <h3>I am Certified by the below following technologies:</h3>
+      <Skill skill="React" emoji="💪" color="blue"/>
+      <Skill skill="Python program fundation" emoji="👍" color="orange"/>
+      <Skill skill="SQL" emoji="💪" color="pink"/>
+      <Skill skill="JavaScript" emoji="💪" color="orangered"/>
+      <Skill skill="HTML+CSS" emoji="👍" color="yellow"/>
+      <Skill skill="Python Automation" emoji="👍" color="Grey"/>
+      <Skill skill="Manual Testing" emoji="👍" color="skyblue"/>
+    </div>
+  )
+}
+
+function Skill(props){
+  return(
+    <div className="skill" style={{backgroundColor:props.color}}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
